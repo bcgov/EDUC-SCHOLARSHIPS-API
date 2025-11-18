a829cda6b4ce7a1e8b6278e783d000263935702e
arcshiftsolutions <arcshiftsolutions@gmail.com>
2025-11-17 18:50:20 -0800

    Script updates

--- api/src/main/resources/scripts/Scholarships to TRAX Data Pump.sql
+++ api/src/main/resources/scripts/Scholarships to TRAX Data Pump.sql
@@ -1,18 +1,68 @@
 TRUNCATE TABLE STUDENT@sldlink.world;
+TRUNCATE TABLE SCHOOL_FUNDING_MASTER@sldlink.world;
+
+CREATE TABLE SCHOOL_FUNDING_GROUP_CODE (
+    SCHOOL_FUNDING_GROUP_CODE VARCHAR(10) NOT NULL,
+    LABEL VARCHAR(30) NOT NULL,
+    DESCRIPTION VARCHAR(255) NOT NULL,
+    LEGACY_CODE VARCHAR(10) NULL
+);
+
+--Create tables in the migration schema
+CREATE TABLE SDC_SCHOOL_COLLECTION
+(
+    SCHOOL_ID VARCHAR2(40),
+    COLLECTION_ID VARCHAR2(40),
+    SDC_SCHOOL_COLLECTION_ID VARCHAR2(40)
+);
+
+--Create tables in the migration schema
+CREATE TABLE INDEPENDENT_SCHOOL_FUNDING_GROUP_SNAPSHOT
+(	    
+     COLLECTION_ID VARCHAR2(40),
+     SCHOOL_ID VARCHAR2(40),
+     SCHOOL_GRADE_CODE VARCHAR2(10),
+     SCHOOL_FUNDING_GROUP_CODE VARCHAR2(10)
+);
 
 --Copy tables into the migration schema with the following fields
-SELECT sdc_student.* FROM SDC_SCHOOL_COLLECTION_STUDENT sdc_student, SDC_SCHOOL_COLLECTION school
+SELECT
+    sdc_student.ASSIGNED_PEN,
+    sdc_student.ENROLLED_GRADE_CODE,
+    sdc_student.SCHOOL_FUNDING_CODE,
+    sdc_student.DOB,
+    sdc_student.FTE,
+    sdc_student.SDC_SCHOOL_COLLECTION_ID,
+    sdc_student.SDC_SCHOOL_COLLECTION_STUDENT_STATUS_CODE
+FROM SDC_SCHOOL_COLLECTION_STUDENT sdc_student, SDC_SCHOOL_COLLECTION school
 WHERE sdc_student.SDC_SCHOOL_COLLECTION_ID = school.SDC_SCHOOL_COLLECTION_ID
-AND school.COLLECTION_ID = :collectionID;
+  AND school.COLLECTION_ID = :collectionID;
 
-SELECT * FROM SDC_SCHOOL_COLLECTION school
+SELECT
+    school.collection_id,
+    school.school_id,
+    school.sdc_school_collection_id
+FROM SDC_SCHOOL_COLLECTION school
 WHERE school.COLLECTION_ID = :collectionID;
 
-SELECT * FROM INDEPENDENT_SCHOOL_FUNDING_GROUP_SNAPSHOT funding
-WHERE funding.COLLECTION_ID = :collectionID;
+SELECT 
+    collection_id, 
+    school_id, 
+    school_grade_code, 
+    school_funding_group_code
+FROM INDEPENDENT_SCHOOL_FUNDING_GROUP_SNAPSHOT funding
+where collection_id =  :collectionID;
+
+--Copy SCHOOL_FUNDING_GROUP_CODE table from SDC API into the migration schema
+select
+    school_funding_group_code,
+    label,
+    description,
+    legacy_code
+from school_funding_group_code;
 
 --Copy both DISTRICT and SCHOOL tables from the target environment into the migration schema
---Copy both SCHOOL_FUNDING_GROUP_CODE table from SDC API into the migration schema
+
 
 --Run in migration schema
 CREATE TABLE SLD_STUDENT_SCHOLARSHIPS_PUMP
@@ -25,18 +75,14 @@ SELECT
     sdc_student.ENROLLED_GRADE_CODE as ENROLLED_GRADE_CODE,
     sdc_student.SCHOOL_FUNDING_CODE as SCHOOL_FUNDING_CODE,
     sdc_student.DOB as BIRTH_DATE,
-    sdc_student.FTE * 100 as FTE,
-    :enterReportDate as REPORT_DATE,
-    'SCHOLARSHIPS' as CREATE_USER,
-    sdc_student.CREATE_DATE as CREATE_DATE,
-    'SCHOLARSHIPS' as UPDATE_USER,
-    sdc_student.UPDATE_DATE as UPDATE_DATE
+    sdc_student.FTE * 100 as STUDENT_FTE_VALUE,
+    :enterReportDate as REPORT_DATE
 FROM SDC_SCHOOL_COLLECTION_STUDENT sdc_student, SDC_SCHOOL_COLLECTION sdc_school
 WHERE sdc_student.SDC_SCHOOL_COLLECTION_ID = sdc_school.SDC_SCHOOL_COLLECTION_ID
 AND sdc_school.COLLECTION_ID = :collectionID
 AND sdc_student.SDC_SCHOOL_COLLECTION_STUDENT_STATUS_CODE = 'COMPLETED';
 
-TRUNCATE TABLE SCHOOL_FUNDING_MASTER@sldlink.world;
+-----------------------------------------
 
 --Run in migration schema
 CREATE TABLE SCHOOL_FUNDING_MASTER_PUMP
@@ -47,26 +93,69 @@ WITH funding_counts AS (
         SCHOOL_FUNDING_GROUP_CODE,
         COUNT(*) AS code_count,
         ROW_NUMBER() OVER (
-            PARTITION BY school_ID 
+            PARTITION BY SCHOOL_ID 
             ORDER BY COUNT(*) DESC, SCHOOL_FUNDING_GROUP_CODE
         ) AS rn
     FROM INDEPENDENT_SCHOOL_FUNDING_GROUP_SNAPSHOT
     WHERE collection_id = :collectionID
-    AND SCHOOL_GRADE_CODE in ('GRADE11','GRADE12')
-    GROUP BY school_ID, SCHOOL_FUNDING_GROUP_CODE
+      AND school_grade_code in ('GRADE11','GRADE12')
+    GROUP BY SCHOOL_ID, SCHOOL_FUNDING_GROUP_CODE
 )
 SELECT
-    (SELECT dist.DISTRICT_NUMBER from DISTRICT dist, SCHOOL school WHERE school.SCHOOL_ID = fund_counts.SCHOOL_ID AND dist.DISTRICT_ID = school.DISTRICT_ID) as DISTNO,
-    (SELECT school.SCHOOL_NUMBER from SCHOOL school WHERE school.SCHOOL_ID = fund_counts.SCHOOL_ID) as SCHLNO,
-    (SELECT fund_group.LEGACY_CODE from SCHOOL_FUNDING_GROUP_CODE fund_group WHERE fund_group.SCHOOL_FUNDING_GROUP_CODE = fund_counts.SCHOOL_FUNDING_GROUP_CODE) as FUNDING_GROUP_CODE,
+    (SELECT dist.DISTRICT_NUMBER from DISTRICT dist, SCHOOL school WHERE school.SCHOOL_ID = fund_counts.school_id AND dist.DISTRICT_ID = school.DISTRICT_ID) as DISTNO,
+    (SELECT school.SCHOOL_NUMBER from SCHOOL school WHERE school.SCHOOL_ID = fund_counts.school_id) as SCHLNO,
+    (SELECT fund_group.legacy_code from SCHOOL_FUNDING_GROUP_CODE fund_group WHERE fund_group.school_funding_group_code = fund_counts.school_funding_group_code) as FUNDING_GROUP_CODE,
     '11' AS FUNDING_GROUP_SUBCODE,
     '20250101' as CREATE_DATE,
     '11134847' as CREATE_TIME,
     'SCHOLARS' as CREATE_USERNAME,
     '20250101' as EDIT_DATE,
     'SCHOLARS' as EDIT_TIME,
-    EDIT_USERNAME
+    'SCHOLARS' as EDIT_USERNAME
 FROM FUNDING_COUNTS fund_counts
 WHERE rn = 1
-ORDER BY school_ID;
+ORDER BY school_id;
 
+----------------------------------------
+
+DECLARE
+v_batch_size NUMBER := 1000;
+  v_counter NUMBER := 0;
+BEGIN
+FOR rec IN (
+    SELECT
+      STUDENT_ID, DISTNO, SCHLNO, REPORT_DATE, ENROLLED_GRADE_CODE,
+      STUDENT_FTE_VALUE, BIRTH_DATE, PEN, SCHOOL_FUNDING_CODE
+    FROM SLD_STUDENT_SCHOLARSHIPS_PUMP
+  ) LOOP
+    INSERT INTO STUDENT@SLDLINK.WORLD VALUES (
+      rec.STUDENT_ID, rec.DISTNO, rec.SCHLNO, rec.REPORT_DATE,
+      NULL, NULL, rec.ENROLLED_GRADE_CODE, rec.STUDENT_FTE_VALUE,
+      NULL, NULL, NULL, NULL, NULL, rec.BIRTH_DATE, NULL, NULL,
+      NULL, NULL, NULL, NULL, NULL, NULL, NULL, rec.PEN, NULL,
+      rec.SCHOOL_FUNDING_CODE, NULL, NULL, NULL, NULL, NULL,
+      NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+    );
+    
+    v_counter := v_counter + 1;
+    
+    IF MOD(v_counter, v_batch_size) = 0 THEN
+      COMMIT;
+      DBMS_OUTPUT.PUT_LINE('Committed ' || v_counter || ' rows');
+END IF;
+END LOOP;
+
+COMMIT; -- Final commit for remaining rows
+DBMS_OUTPUT.PUT_LINE('Total rows inserted: ' || v_counter);
+END;
+
+
+---------------------------------------------
+SELECT
+    'UPDATE SCHOOL_FUNDING_MASTER@spmlink.world SET FUNDING_GROUP_CODE=''' || sfm.FUNDING_GROUP_CODE ||
+    ''' WHERE DISTNO=''' || sfm.DISTNO ||
+    ''' AND FUNDING_GROUP_SUBCODE=''' || sfm.FUNDING_GROUP_SUBCODE ||
+    ''' AND SCHLNO=''' || sfm.SCHLNO || ''';'
+FROM SCHOOL_FUNDING_MASTER_PUMP sfm;
+
+--Take update statements and run them
